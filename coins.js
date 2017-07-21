7aceaf669d652ec9b0770e5741f3cb6b2f297c74
Nora Moser <noramoser@Noras-MacBook-Pro.local>
2017-07-21 10:26:30 -0500

    finished

--- coins.js
+++ coins.js
@@ -1,52 +1,62 @@
 
 // let coinPurse = 0;
 
-let quarters = .25;
-let dimes = .1;
-let nickels = .05;
-let pennies = .01;
+let quarters = 25;
+let dimes = 10;
+let nickels = 5;
+let pennies = 1;
 
 // let money = [quarters, dimes, nickels, pennies];
 
 // function coinCounter(Placeholder){
 // 	placeholder = (coinPurse / money);
 // }
-let input = prompt("Give me an amount of money from one penny to one dollar.");
+let input = prompt("Give me an amount of money from one penny to one dollar.") * 100;
 // console.log(input);
 // let output = {};
 let coinPurse = {};
 
-function coinCounter () {
-  // Initialize a JavaScript object to hold the coins
-  
   coinPurse.quarters = 0;
   coinPurse.dimes = 0;
   coinPurse.nickels = 0;
   coinPurse.pennies = 0;
 
+function coinCounter () {
+  // Initialize a JavaScript object to hold the coins
+  
+
+// for (let i = 0; i < coinPurse.length; i++) {
+//   while (coinPurse >= .01){
+// 	}
+// }
+
 let arrayQuarters = input / quarters;
-let arrayDimes = input / dimes;
-let arrayNickels = input / nickels;
-let arrayPennies = input / pennies;
+let arrayDimes = ((input % quarters) / dimes); 
+let arrayNickels = ((input % quarters) % dimes) / nickels;
+let arrayPennies = ((((input % quarters) % dimes)) % nickels) / pennies;
+
+
 
   if (arrayQuarters >= 1) {
-  	coinPurse.quarters += 1;
-  } else if (arrayDimes >= 1) {
-  	coinPurse.dimes += 1;
-  } else if (arrayNickels >= 1) {
-    coinPurse.nickels += 1;
-  } else if (arrayPennies >= 1) {
-  	coinPurse.pennies += 1;
-  }
-
+  	coinPurse.quarters += Math.floor(arrayQuarters);
+   } if (arrayDimes >= 1) {
+  	  coinPurse.dimes += Math.floor(arrayDimes);
+   }   if (arrayNickels >= 1) {
+	      coinPurse.nickels += Math.floor(arrayNickels);
+   }     if (arrayPennies >= 1) {
+  	   coinPurse.pennies += Math.floor(arrayPennies);
+   }
+  // // if (arrayDimes >= 1) {
+  // // 	coinPurse.dimes += Math.floor(arrayDimes);
+  // }
 
   return coinPurse;
 
 
 }
 
-let coin = coinCounter()
-console.log(coin);
+let coin = coinCounter();
+console.log(coin)
 
 
 
